06de1fe23639bbe1b992a19df7748891f46548ce
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-14 14:12:43 +0500

    Comleting Random Generator Color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Accordian from './components/accordians/Accordian'
+import RandomColor from './components/randomColor/RandomColor'
 
 const App = () => {
   return (
     <div>      
       <Accordian />
+      <RandomColor />
     </div>
   )
 }
---
+++ src/components/randomColor/RandomColor.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+export default function RandomColor() {
+
+    const [typeOfColor, setTypeOfColor] = useState('hex');
+    const [color, setColor] = useState('#000000');
+
+    function randomColorUtility(length) {
+        return Math.floor(Math.random() * length);
+      }
+
+    function handleCreateRandomColorHEX(){
+        const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+        let hexColor = "#";
+        for(let i = 0; i < 6; i++){
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+        console.log(hexColor);
+        setColor(hexColor);
+
+    }
+
+    function handleCreateRandomColorRGB(){
+        const r = randomColorUtility(256);
+        const g = randomColorUtility(256);
+        const b = randomColorUtility(256);
+
+        setColor(`rgb(${r}, ${g}, ${b})`);
+    }
+
+  return (
+    <div style={{width:'100vw', height: '100vh', background: color }}>
+        <button 
+            onClick={() => setTypeOfColor('hex')}
+            style={{margin: '50px', padding: '4px', borderRadius: '4px', backgroundColor: 'lightblue'}}>
+            Create HEX Colors
+        </button>
+        <button 
+            onClick={() => setTypeOfColor('rgb')}
+            style={{margin: '50px', padding: '4px', borderRadius: '4px', backgroundColor: 'lightyellow'}}>
+            Create RGB Colors
+        </button>
+        <button 
+            onClick={typeOfColor === 'hex' ?  handleCreateRandomColorHEX : handleCreateRandomColorRGB }
+            style={{margin: '50px', padding: '4px', borderRadius: '4px', backgroundColor: 'lightgreen'}}>
+            Genrate the Random Color
+        </button>
+        <h3>{typeOfColor === "rgb" ? "RGB Color" : "HEX Color"}</h3>
+        <h1>{color}</h1>
+    </div>
+  )
+}
