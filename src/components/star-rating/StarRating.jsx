94cce1de785643027b543a4a2aeaa3b3b868df51
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-14 19:05:34 +0500

    Styling Stars

--- src/components/star-rating/StarRating.jsx
+++ src/components/star-rating/StarRating.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import {FaSrar} from 'react-icons/fa';
+import React, { useState } from 'react'
+import {FaStar} from 'react-icons/fa';
+import './style.css';
 
 export default function StarRating({noOfStars = 5}) {
+    
+    const [rating, setRating] = useState(0);
+    const [hovering, setHovering] = useState(0);
+
+ 
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex);
+    }
+
+    function handleMouseEnter(getCurrentIndex)  {
+        setHovering(getCurrentIndex);
+    }
+
+    function handleMouseLeave(getCurrentIndex){
+        setHovering(rating);
+    }
+
   return (
     <div className='star-rating'>
         {
             [...Array(noOfStars)].map((_, index) =>{
                 index += 1;
-                return <FaSrar
-                    key = {index}
-                    onClick={() => handleClick(index)}
-                    onMouseMove={() => handleMouseEnter(index)}
-                    onMouseLeave={() => handleMouseLeave()}
-                    size = {40}                    
-                 />
+                return (
+                    <FaStar
+                        className={index <= (hovering || rating) ? "active" : "inactive"}
+                        key = {index}
+                        onClick = {() => handleClick(index)}
+                        onMouseMove={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave()}
+                        size = {40}                    
+                    />
+                );
             })
         }
     </div>
