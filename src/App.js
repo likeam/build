afe443857d158813b22ee4941065269de97be162
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-16 09:58:28 +0500

    Tree View initial setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordian from './components/accordians/Accordian'
 import RandomColor from './components/randomColor/RandomColor'
 import StarRating from './components/star-rating/StarRating'
 import ImageSlider from './components/imageSlider/ImageSlider'
+import TreeView from './components/treeView/TreeView'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <ImageSlider  url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"} />
+      <TreeView />
     </div>
   )
 }
