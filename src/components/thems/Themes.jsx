620a3070b9afbb8abe4179d2292bb2be9b9ab60f
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-25 10:57:07 +0500

    Css for theme

--- src/components/thems/Themes.jsx
+++ src/components/thems/Themes.jsx
@@ -1,13 +1,21 @@
 
-import React from 'react'
+
 import './style.css'
+import useLocalStorage from './useLocalStorage'
 
 export default function Themes() {
+
+    const [theme, setTheme] = useLocalStorage('theme', 'dark');
+
+    function handleToggleThemes(){
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
   return (
-    <div className="light-dark-mode" >
+    <div className="light-dark-mode" data-theme={theme} >
       <div className="container">
         <p>Hello World !</p>
-        <button >Change Theme</button>
+        <button onClick={handleToggleThemes} >Change Theme</button>
       </div>
     </div>
   )
